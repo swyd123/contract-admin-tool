ad92164d6e369b51892c96246184e54e3e004887
swyd123 <sarah@reggioacademy.com.au>
2026-06-16 12:20:52 +1000

    Update supabase_client.py

--- utils/supabase_client.py
+++ utils/supabase_client.py
@@ -1,1 +1,16 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client
 
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
+
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
+
+def get_supabase():
+    return supabase
